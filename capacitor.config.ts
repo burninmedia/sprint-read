c1b98401b88299060317ba429cd2c36e53adc537
Stephen_Lester <stephenl@burnin-media.com>
2026-04-14 11:43:09 -0400

    update app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.sprintread.app',
+  appId: 'com.burninmedia.sprintread.app',
   appName: 'SprintRead',
   webDir: 'dist',
   server: {
